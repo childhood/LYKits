0aac211333f210a50df95bae01116a61ba8a9e9e
Leo <superartstudio@gmail.com>
2011-08-08 23:38:28 +0800

    db30

--- LYServiceKit/LYServiceKit.m
+++ LYServiceKit/LYServiceKit.m
@@ -13,6 +13,7 @@
 		[data key:@"host"		v:@"http://super-db.appspot.com/rest"];
 		[data key:@"username"	v:@"test"];
 		[data key:@"password"	v:@"passwordtest"];
+		[data key:@"scheme"		v:[NSMutableDictionary dictionary]];
 	}
 	return self;
 }
@@ -85,7 +86,6 @@
 							 [data v:@"username"],
 							 [data v:@"password"]];
 	__block ASIFormDataRequest *request = [ASIFormDataRequest requestWithURL:[NSURL URLWithString:url]];
-	//NSLog(@"url: %@", url);
 	[request addRequestHeader:@"Content-Type" value:@"application/json"];
 	//[request setPostValue:@"application/json" forKey:@"Content-Type"];
 	//[request setPostValue:[data v:@"username"] forKey:@"username"];
@@ -93,6 +93,7 @@
 	[request appendPostData:[[CJSONSerializer serializer] serializeDictionary:dict error:nil]];
 	[request setRequestMethod:@"POST"];
 #if 0
+	NSLog(@"url: %@", url);
 	NSLog(@"request: %@", request);
 	NSLog(@"request: %@", request.requestMethod);
 	NSLog(@"request: %@", request.postBody);
@@ -102,7 +103,7 @@
 #endif
 	//[request appendPostData:[@"This is my data" dataUsingEncoding:NSUTF8StringEncoding]];
 	[request setCompletionBlock:^{
-		//	NSLog(@"headers: %@", request.responseHeaders);
+			NSLog(@"headers: %@", request.responseHeaders);
 		//	NSLog(@"response: %@", request.responseString);
 		NSArray* contents = [request.responseString componentsSeparatedByString:@","];
 		//	NSLog(@"contents: %@", contents);
@@ -113,6 +114,72 @@
 		callback(nil, request.error);
 	}];
 	[request startAsynchronous];
+}
+
+- (void)set_scheme_user
+{
+	[[data v:@"scheme"] key:@"user" v:[NSDictionary dictionaryWithObjectsAndKeys:
+		@"db30_model",
+		@"database",
+		[NSArray arrayWithObjects:
+			@"email",
+			@"name",
+			nil],
+		@"s",
+		[NSArray arrayWithObjects:
+			@"friends",
+			nil],
+		@"t",
+		nil]];
+}
+
+- (void)sdb:(NSString*)dbname insert:(NSArray*)source block:(void (^)(NSArray* keys, NSError* error))callback
+{
+	NSDictionary* dict			= [[data v:@"scheme"] v:dbname];
+	NSMutableDictionary* dest	= [NSMutableDictionary dictionary];
+	int index;
+	NSString* type;
+
+	[dest key:@"name" v:dbname];
+	[dest key:@"username" v:[data v:@"username"]];
+	[dest key:@"password" v:[data v:@"password"]];
+#if 0
+	int i, count;
+	if ([[dict v:@"database"] is:@"db30_model"])
+		count = 30;
+	for (i = 0; i < count; i++)
+	{
+		[dest key:[NSString stringWithFormat:@"s%i", i] v:@""];
+		[dest key:[NSString stringWithFormat:@"t%i", i] v:@""];
+	}
+#endif
+	NSLog(@"scheme: %@", dict);
+	for (NSDictionary* dict_item in source)
+	{
+		for (NSString* key in dict_item)
+		{
+			id obj = [dict_item v:key];
+			if ([obj isKindOfClass:[NSString class]])
+				type = @"s";
+			else if ([obj isKindOfClass:[NSArray class]])
+			{
+				obj = [NSString stringWithUTF8String:[[[CJSONSerializer serializer] serializeArray:obj error:nil] bytes]];
+				type = @"t";
+			}
+			index = [[dict v:type] indexOfObject:key];
+			NSLog(@"%i: %@", index, key);
+			[dest key:[NSString stringWithFormat:@"%@%i", type, index] v:obj];
+		}
+	}
+	NSLog(@"dest: %@", dest);
+#if 1
+	[self name:[dict v:@"database"] insert:[NSArray arrayWithObjects:dest,
+		//[NSDictionary dictionaryWithObjectsAndKeys:@"id-006", @"id", @"desc-006", @"desc", @"data-006", @"data", nil],
+		nil] block:^(NSArray* array, NSError* error)
+	{
+		NSLog(@"add user result: %@ - %@", error, array);
+	}];
+#endif
 }
 
 - (void)test
@@ -136,27 +203,25 @@
 		NSLog(@"result: %@ - %@", error, array);
 	}];
 #endif
-#if 0
-	//	set scheme - should be hard coded in application abstract layer, dbxxx does not care about all these
-	[[db.data v:@"scheme"] key:@"user" v:[NSArray arrayWithObjects:
-		@"email",
-		@"name",
-		@"friends",
-		nil]];
+#if 1
 	//	insert into dbxxx (with scheme already given)
-	[db db30:@"user" insert:[NSArray arrayWithObjects:
+	[db set_scheme_user];
+	[db sdb:@"user" insert:[NSArray arrayWithObjects:
 		[NSDictionary dictionaryWithObjectsAndKeys:
 			@"no@name.com",
 			@"email",
-			@"Leo",
+			@"Leo.002",
 			@"name",
-			[NSMutableArray array],
+			[NSMutableArray arrayWithObjects:
+				@"noa@name.com",
+				@"nob@name.com",
+				nil],
 			@"friends",
 			nil],
 		nil] block:nil];
 #endif
 #if 0
-	[db db30:@"user" select:@"" block:nil];
+	[db db30_model:@"user" select:@"" block:nil];
 #endif
 	[db release];
 }
