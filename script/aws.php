c75c5c0208c7558cf2155a3d4c9ef90b1d87037a
Leo <leo@superarts.org>
2012-01-18 08:42:01 +0800

    script

--- script/aws.php
+++ script/aws.php
@@ -19,6 +19,35 @@ function cmd_input($prompt = 'Value')
 	return $ret;
 }
 
+function loop_select($query)
+{
+	global $sdb;
+	$result = $sdb->select($query);
+	$result = reorganize_data($result->body->Item());
+	sort($result['columns']);
+	deep_ksort($result['rows']);
+	echo "---- columns ----\n";
+	print_r($result['columns']);
+	echo "---- rows ----\n";
+	if (count($array) > 0)
+	{
+		foreach ($result['rows'] as $array)
+		{
+			foreach ($array as $key => $attrs)
+			{
+				echo str_pad("$key:", 16, ' ', STR_PAD_RIGHT);
+				if (is_array($attrs))
+					foreach ($attrs as $attr)
+						echo "  $attr\n";
+				else
+					echo "  $attrs\n";
+			}
+			echo "----\n";
+		}
+	}
+	echo "---- end ----\n";
+}
+
 //	php test.php select 'count(*) from `posts` where `pos-a7` = "Australia"'
 
 define('AWS_KEY', 'AKIAIG737NOEC2VVPXQQ');
@@ -27,7 +56,8 @@ define('AWS_SECRET_KEY', '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');
 $process_user	= false;
 $process_photo	= true;
 
-require('/Users/leo/prj/sdk/aws/sdk-1.4.3/sdk-1.4.3/sdk.class.php');
+//require('/Users/leo/prj/sdk/aws/sdk-1.4.3/sdk-1.4.3/sdk.class.php');
+require('/Users/leo/prj/sdk/aws/sdk-1.5.0.1/sdk-1.5.0.1/sdk.class.php');
 $sdb	= new AmazonSDB();
 $s3		= new AmazonS3();
 
@@ -40,9 +70,14 @@ if ($argc > 1)
 	case 'help':
 		echo "aws.php \ select * from posts limit 1\n";
 		echo "aws.php all \ users limit 1\n";
+		echo "aws.php select * from posts where ...\n";
 		echo "aws.php put test name001 key1 value1\n";
 		echo "aws.php replace test name001 key1 value0\n";
 		echo "aws.php delete test name001\n";
+		echo "\tEMPTY: repeat last command\n";
+		echo "\tr: display 10 blank lines\n";
+		echo "\tq: quit\n";
+		echo "\n";
 		die;
 	case 'all':
 		$query_prefix = 'select * from ';
@@ -55,6 +90,13 @@ if ($argc > 1)
 		die;
 	case 'delete':
 		$sdb->delete_attributes($argv[2], $argv[3]);
+		die;
+	case 'select':
+		$query = '';
+		for ($i = 1; $i < $argc; $i++)
+			$query .= $argv[$i] . ' ';
+		//	echo "$query\n";
+		loop_select($query);
 		die;
 	/*
 	case 'select':
@@ -71,32 +113,22 @@ if ($argc > 1)
 	}
 }
 
+$last_query = 'select * from posts limit 2';
 do {
 	$query = cmd_input('query');
 	if ($query == '')
+		$query = $last_query;
+	else if ($query == 'q')
 		die;
-	//	echo "$query\n";
-	$result = $sdb->select($query_prefix . $query);
-	$result = reorganize_data($result->body->Item());
-	sort($result['columns']);
-	deep_ksort($result['rows']);
-	echo "---- columns ----\n";
-	print_r($result['columns']);
-	echo "---- rows ----\n";
-	foreach ($result['rows'] as $array)
+	else if ($query == 'r')
 	{
-		foreach ($array as $key => $attrs)
-		{
-			echo str_pad("$key:", 16, ' ', STR_PAD_RIGHT);
-			if (is_array($attrs))
-				foreach ($attrs as $attr)
-					echo "  $attr\n";
-			else
-				echo "  $attrs\n";
-		}
-		echo "----\n";
+		echo "\n\n\n\n\n\n\n\n\n\n";
+		continue;
 	}
-	echo "---- end ----\n";
+	else
+		$last_query = $query;
+	//	echo "$query_prefix$query\n";
+	loop_select($query_prefix . $query);
 }	while ($query != '');
 die;
 
@@ -193,38 +225,41 @@ function reorganize_data($items)
 	$columns = array();
 
 	// Loop through each of the items
-	foreach ($items as $item)
+	if (count($item) > 0)
 	{
-		// Let's append to a new row
-		$row = array();
-		$row['id'] = (string) $item->Name;
-
-		// Loop through the item's attributes
-		foreach ($item->Attribute as $attribute)
+		foreach ($items as $item)
 		{
-			// Store the column name
-			$column_name = (string) $attribute->Name;
+			// Let's append to a new row
+			$row = array();
+			$row['id'] = (string) $item->Name;
 
-			// If it doesn't exist yet, create it.
-			if (!isset($row[$column_name]))
+			// Loop through the item's attributes
+			foreach ($item->Attribute as $attribute)
 			{
-				$row[$column_name] = array();
+				// Store the column name
+				$column_name = (string) $attribute->Name;
+
+				// If it doesn't exist yet, create it.
+				if (!isset($row[$column_name]))
+				{
+					$row[$column_name] = array();
+				}
+
+				// Append the new value to any existing values
+				// (Remember: Entries can have multiple values)
+				$row[$column_name][] = (string) $attribute->Value;
+				natcasesort($row[$column_name]);
+
+				// If we've not yet collected this column name, add it.
+				if (!in_array($column_name, $columns, true))
+				{
+					$columns[] = $column_name;
+				}
 			}
 
-			// Append the new value to any existing values
-			// (Remember: Entries can have multiple values)
-			$row[$column_name][] = (string) $attribute->Value;
-			natcasesort($row[$column_name]);
-
-			// If we've not yet collected this column name, add it.
-			if (!in_array($column_name, $columns, true))
-			{
-				$columns[] = $column_name;
-			}
+			// Append the row we created to the list of rows
+			$rows[] = $row;
 		}
-
-		// Append the row we created to the list of rows
-		$rows[] = $row;
 	}
 
 	// Return both
