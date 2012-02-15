eb1d014954dc0bc4a0f0983fe15fe5509caf807b
Leo <superartstudio@gmail.com>
2012-02-15 23:26:39 +0800

    revert

--- LYServiceKit/LYServiceUI.m
+++ LYServiceKit/LYServiceUI.m
@@ -35,7 +35,7 @@
 - (IBAction)load_wall
 {
 	NSString* query = [NSString stringWithFormat:
-		@"select * from `posts` where `author-mail` = '%@' and `date-create` < '21000101-00:00:00' and `date-create` > '20110101-00:00:00' order by `date-create` desc limit 5",
+		@"select * from `posts` where `author-mail` = '%@' and `date-create` < '21000101-00:00:00' and `date-create` > '20110101-00:00:00' order by `date-create` desc",
 		[@"ly-suar-profile-mail" setting_string]];
 	NSLog(@"load wall: %@", query);
 	[self reload_provider:&provider_wall table:table_wall query:query sdb:sdb_wall data:array_wall];
